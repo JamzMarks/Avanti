6f3802e51e6f2007f9c3460d27341dbf1eb3aace
JamzMarks <jamzmarks@gmail.com>
2025-04-24 16:04:07 -0300

    update newletter

--- src/newsletterForm.js
+++ src/newsletterForm.js
@@ -2,9 +2,10 @@ import { validateEmail } from "../utils/validator/email-validator.js";
 
 export function handleNewsletterSubmit(event){
     event.preventDefault();
+    const name = document.getElementById("NLNameInput").value;
     const email = document.getElementById("NLEmailInput").value;
     const validEmail = validateEmail(email);
-    if (validEmail) {
+    if (validEmail && name != "") {
         alert('Um link de confirmação foi enviado para o seu e-mail. Por favor, verifique sua caixa de entrada.');
     } else {
         alert('Insira um e-mail válido!');
