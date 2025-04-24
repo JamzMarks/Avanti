234740ffd68d6e101a3714c2e1a2e864996df410
JamzMarks <jamzmarks@gmail.com>
2025-04-24 09:57:02 -0300

    Fix: navigation grif

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { handleNewsletterSubmit } from "./src/components/newsletterForm.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   //Menu hamburguer button
-  document.getElementById("menuButton").addEventListener("click", function () {
-    let checkbox = document.getElementById("menuCheckbox");
-    checkbox.checked = !checkbox.checked;
-  });
+  // document.getElementById("menuButton").addEventListener("click", function () {
+  //   let checkbox = document.getElementById("menuCheckbox");
+  //   checkbox.checked = !checkbox.checked;
+  // });
 
   //newsletterForm
   document
